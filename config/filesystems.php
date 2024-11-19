ba03833ee4b26fa029746fbc140cc1d365447bbc
kenshu <kenshu.app@gmail.com>
2024-11-19 14:13:19 +0900

    add: profilePhoto

--- config/filesystems.php
+++ config/filesystems.php
@@ -39,7 +39,8 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            // 'url' => env('APP_URL').'/storage',
+            'url' => url('').'/storage',
             'visibility' => 'public',
             'throw' => false,
         ],
